a97b8c12244874a61d421174373b09ae281dc4f2
Dan Duvall <dduvall@wikimedia.org>
2023-03-01 11:11:29 -0800

    Revert "Add workaround for Istio/buildkitd network conflict"
    This reverts commit d8320ac9633fb0d76b6691e3ec4305b3dfe407af.

--- wmf/entrypoint-privileged.sh
+++ wmf/entrypoint-privileged.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-set -eu -o pipefail
-
-if [ -f /etc/buildkit/cni.json ]; then
-    type=$(jq -r .type /etc/buildkit/cni.json)
-    if [ "$type" == "bridge" ]; then
-        name=$(jq -r .bridge /etc/buildkit/cni.json)
-
-        # Ensure that Istio can't get its hand on outbound traffic
-        # from build containers before masquerading. (T330433)
-        echo Running iptables-legacy hack for https://phabricator.wikimedia.org/T330433
-        /usr/sbin/iptables-legacy -t nat -I PREROUTING -i "$name" -j RETURN
-    fi
-fi
-
 exec /usr/local/sbin/buildkitd \
   --oci-worker true \
   --oci-worker-gc \
